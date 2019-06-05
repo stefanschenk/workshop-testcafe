2e35dea35ea70fcf2761b8777d790441954acd94
Burak Yurdakul <b.yurdakul@portofrotterdam.com>
2019-06-05 15:51:58 +0200

    adjustes some instructions

--- specs/contacts.spec.ts
+++ specs/contacts.spec.ts
@@ -8,17 +8,19 @@ fixture`Contacts`
     .page`http://localhost:3000/contacts`
     .beforeEach(async t => {
         await t
-            .useRole(User.contractsUser)
             /**
              * The login is covered by using a Role in Testcafe.
              * Take a look at user.seq.ts to find out how that works.
-             *
+             */
+            .useRole(User.contractsUser)
+            /**
              * As explained during the presentation, testcafe supports interaction with the browser.
              * For this, ClientFunctions are used.
              * 
-             * {@link Browser.getLocation()} returns an empty string for now.
+             * The ClientFunction {@link Browser.getLocation()} returns an empty string for now.
+             * Adjust the function so that it can fetch the current URL.
              */
-            .expect(await Browser.getLocation()).contains('contacts')
+            .expect(await Browser.getLocation()).contains('contacts');
     });
 
 test('Create a new contact with name and e-mail', async t => {
@@ -28,7 +30,7 @@ test('Create a new contact with name and e-mail', async t => {
          *
          * The assertion of a succesfull creation is already given.
          */
-        .expect(contacts.successMessage).eql('Contact Saved')
+        .expect(contacts.successMessage).eql('Contact Saved');
 });
 
 test('Update newly created contact with a new phone number', async t => {
@@ -38,7 +40,7 @@ test('Update newly created contact with a new phone number', async t => {
          *
          * The assertion of a succesfull update is already given.
          */
-        .expect(contacts.successMessage).eql('Contact Updated')
+        .expect(contacts.successMessage).eql('Contact Updated');
 });
 
 test('Delete newly created contact', async t => {
@@ -48,5 +50,5 @@ test('Delete newly created contact', async t => {
          *
          * The assertion of a succesfull deletion is already given.
          */
-        .expect(contacts.successMessage).eql('Contact Removed')
+        .expect(contacts.successMessage).eql('Contact Removed');
 });
